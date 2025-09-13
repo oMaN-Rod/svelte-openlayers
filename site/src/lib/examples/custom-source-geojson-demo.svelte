834750bed8f9c0571270496ad87f55af017e43f4
oMaN-Rod <3799811+oMaN-Rod@users.noreply.github.com>
2025-09-12 23:42:33 -0400

    refactor: consolidate vector layer and source initialization in custom GeoJSON demo

--- site/src/lib/examples/custom-source-geojson-demo.svelte
+++ site/src/lib/examples/custom-source-geojson-demo.svelte
@@ -2,36 +2,29 @@
 	import { Map, Layer } from 'svelte-openlayers';
 	import VectorSource from 'ol/source/Vector';
 	import GeoJSON from 'ol/format/GeoJSON';
-	import { mapSources } from './sources';
 	import { Overlay } from 'svelte-openlayers';
+	import VectorLayer from 'ol/layer/Vector';
 
 	let center = $state([0, 0]);
 	let zoom = $state(1);
-	let vectorSource = new VectorSource({
-		url: 'https://openlayers.org/data/vector/ecoregions.json',
-		format: new GeoJSON()
+	const layer = new VectorLayer({
+		background: '#181818',
+		source: new VectorSource({
+			url: 'https://openlayers.org/data/vector/ecoregions.json',
+			format: new GeoJSON()
+		}),
+		style: {
+			'fill-color': ['string', ['get', 'COLOR'], '#eee']
+		}
 	});
 </script>
 
 <div class="h-96 w-full overflow-hidden rounded-lg border">
 	<Map.Root class="h-full w-full">
 		<Map.View bind:center bind:zoom />
-		<Layer.Tile
-			source="xyz"
-			url={mapSources.find((s) => s.id === 'carto-voyager')?.url}
-			attributions={mapSources.find((s) => s.id === 'carto-voyager')?.attributions}
-		/>
+		<Layer.Tile source="osm" />
 
-		{#if vectorSource}
-			<Layer.Vector
-				source={vectorSource}
-				visible={true}
-				opacity={1}
-				style={{
-					'fill-color': ['string', ['get', 'COLOR'], '#eee']
-				}}
-			/>
-		{/if}
+		<Layer.Vector {layer} />
 
 		<Overlay.TooltipManager />
 	</Map.Root>
